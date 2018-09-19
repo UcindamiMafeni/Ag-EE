b122da88fd77761665270b337700b81491f36ce9
lpreonas <lpreonas@berkeley.edu>
2018-09-19 10:50:06 -0700

    GIS script to assign SPs and pumps to California counties

---
+++ build/BUILD_gis_counties.R
@@ -0,0 +1,226 @@
+#######################################################
+#  Script to assign SP premises and APEP pumps to     #
+#  California counties                                #
+####################################################### 
+rm(list = ls())
+
+
+#install.packages("ggmap")
+#install.packages("ggplot2")
+#install.packages("gstat")
+#install.packages("sp")
+#install.packages("maptools")
+#install.packages("rgdal")
+#install.packages("rgeos")
+#install.packages("raster")
+#install.packages("SDMTools")
+
+#libP <- "C:/Program Files/Microsoft/R Open/R-3.4.4/library"
+
+library(ggmap) #, lib.loc=libP)
+library(ggplot2) #, lib.loc=libP)
+library(gstat) #, lib.loc=libP)
+library(sp) #, lib.loc=libP)
+library(maptools) #, lib.loc=libP)
+library(rgdal) #, lib.loc=libP)
+library(rgeos) #, lib.loc=libP)
+library(raster) #, lib.loc=libP)
+library(SDMTools) #, lib.loc=libP)
+
+
+##########################################
+### 1. Prep all relevant in shapefiles ###
+##########################################
+
+setwd("S:/Matt/ag_pump/data/spatial")
+
+#Load Counties shapefile
+counties <- readOGR(dsn = "Counties", layer = "CA_Counties_TIGER2016")
+proj4string(counties)
+counties <- spTransform(counties, CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=GRS80 +towgs84=0,0,0"))
+
+#Load CA state outline
+CAoutline <- readOGR(dsn = "State", layer = "CA_State_TIGER2016")
+proj4string(CAoutline)
+CAoutline <- spTransform(CAoutline, CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=GRS80 +towgs84=0,0,0"))
+
+#Confirm counties align with California map
+ggplot() + 
+  geom_polygon(data=CAoutline, aes(x=long, y=lat, group=group), 
+               color="grey30", fill=NA, alpha=1) +
+  geom_polygon(data=counties, aes(x=long, y=lat, group=group, color=rgb(0,0,1)), 
+               color=rgb(0,1,0), fill=rgb(0,0,1), alpha=1) 
+
+
+#########################################
+### 2. Assign SP lat/lons to counties ###
+#########################################
+
+#Read PGE coordinates
+setwd("S:/Matt/ag_pump/data/misc")
+prems <- read.delim2("pge_prem_coord_3pulls.txt",header=TRUE,sep=",",stringsAsFactors=FALSE)
+prems$longitude <- as.numeric(prems$prem_lon)
+prems$latitude <- as.numeric(prems$prem_lat)
+
+#Convert to SpatialPointsDataFrame
+coordinates(prems) <- ~ longitude + latitude
+proj4string(prems) <- proj4string(counties)
+
+#Assign each lat/lon to the county polygon it's contained in
+prems@data$IN_county <- over(prems, counties)
+
+#Reproject everything into planar coordinates
+utmStr <- "+proj=utm +zone=%d +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0"
+crs <- CRS(sprintf(utmStr, 10))
+countiesUTM <- spTransform(counties, crs) #reproject into planar coordinates, because that's what the distance function uses
+premsUTM <- spTransform(prems, crs)
+
+#Create empy vectors to loop over, to calculate distance to a county
+n <- nrow(prems@data)
+nearestCounty <- character(n)
+nearestCounty_ID <- numeric(n)
+nearestCounty_dist_km <- numeric(n)
+
+#Create vector of only those observations where county is missing
+missings <- c(1:n)[is.na(prems@data$IN_county$COUNTYFP)]
+
+#Calculate distance to a county, for lat/lons not contained in a polygon
+for (j in seq_along(missings)) {
+  i <- missings[j]
+  temp <- countiesUTM@data[which.min(gDistance(premsUTM[i,], countiesUTM, byid=TRUE)),]
+  nearestCounty[i]      <- as.character(temp$NAME)
+  nearestCounty_ID[i]   <- as.numeric(as.character(temp$COUNTYFP))
+  nearestCounty_dist_km[i] <- min(gDistance(premsUTM[i,], countiesUTM, byid=TRUE))/1000
+}
+
+#Convert back to regular dataframe
+prems <- as.data.frame(prems)
+
+#Assign in_county dummy and store county name
+prems$county <- prems$IN_county.NAME
+prems$in_county <- as.numeric(is.na(prems$county)==0)
+prems$county_id <- as.numeric(as.character(prems$IN_county.COUNTYFP))
+summary(prems$in_county)
+summary(prems$in_county[prems$bad_geocode_flag==0])
+
+#Append nearest county variables
+prems <- cbind(prems, nearestCounty, nearestCounty_ID, nearestCounty_dist_km)
+summary(prems[prems$in_county==0,]$nearestCounty_dist_km)
+
+#Plot points in counties
+ggplot() + 
+  geom_polygon(data=CAoutline, aes(x=long, y=lat, group=group), 
+               color="grey30", fill=NA, alpha=1) +
+  geom_polygon(data=counties, aes(x=long, y=lat, group=group), 
+               color="green", fill=NA, alpha=1) +
+  geom_point(data=prems[prems$in_county==1,], aes(x=longitude, y=latitude), color=rgb(0,0,1), shape=19, 
+             alpha=1, size=1) 
+
+#Plot points NOT in counties // all have bad geocodes
+ggplot() + 
+  geom_polygon(data=CAoutline, aes(x=long, y=lat, group=group), 
+               color="grey30", fill=NA, alpha=1) +
+  geom_polygon(data=counties, aes(x=long, y=lat, group=group), 
+               color="green", fill=NA, alpha=1) +
+  geom_point(data=prems[(prems$in_county==0),], aes(x=longitude, y=latitude), color=rgb(0,0,1), shape=19, 
+             alpha=1, size=1) 
+
+#Drop extraneous variables
+prems <- prems[c("sp_uuid","prem_lat","prem_long","longitude","latitude",
+                 "bad_geocode_flag","pull","county","in_county","county_id")]
+
+#Export results to txt
+filename <- "pge_prem_coord_polygon_counties.txt"
+write.table(prems, file=filename , row.names=FALSE, col.names=TRUE, sep="%", quote=FALSE, append=FALSE)
+
+
+##################################################
+### 3. Assign APEP lat/lons to counties ###
+##################################################
+
+#Read APEP coordinates
+setwd("S:/Matt/ag_pump/data/misc")
+pumps <- read.delim2("apep_pump_coord.txt",header=TRUE,sep=",",stringsAsFactors=FALSE)
+pumps$longitude <- as.numeric(pumps$pump_lon)
+pumps$latitude <- as.numeric(pumps$pump_lat)
+
+#Convert to SpatialPointsDataFrame
+coordinates(pumps) <- ~ longitude + latitude
+proj4string(pumps) <- proj4string(counties)
+
+#Assign each lat/lon to the county polygon it's contained in
+pumps@data$IN_county <- over(pumps, counties)
+
+#Reproject everything into planar coordinates
+utmStr <- "+proj=utm +zone=%d +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0"
+crs <- CRS(sprintf(utmStr, 10))
+countiesUTM <- spTransform(counties, crs) #reproject into planar coordinates, because that's what the distance function uses
+pumpsUTM <- spTransform(pumps, crs)
+
+#Create empy vectors to loop over, to calculate distance to a county
+n <- nrow(pumps@data)
+nearestCounty <- character(n)
+nearestCounty_ID <- numeric(n)
+nearestCounty_dist_km <- numeric(n)
+
+#Create vector of only those observations where county is missing
+missings <- c(1:n)[is.na(pumps@data$IN_county$COUNTYFP)]
+
+#Calculate distance to a county polygon, for lat/lons not contained in a polygon
+for (j in seq_along(missings)) {
+  i <- missings[j]
+  temp <- countiesUTM@data[which.min(gDistance(pumpsUTM[i,], countiesUTM, byid=TRUE)),]
+  nearestCounty[i]      <- as.character(temp$NAME)
+  nearestCounty_ID[i]   <- as.numeric(as.character(temp$COUNTYFP))
+  nearestCounty_dist_km[i] <- min(gDistance(pumpsUTM[i,], countiesUTM, byid=TRUE))/1000
+}
+
+#Convert back to regular dataframe
+pumps <- as.data.frame(pumps)
+
+#Assign in_county dummy and store county name
+pumps$county <- pumps$IN_county.NAME
+pumps$in_county <- as.numeric(is.na(pumps$county)==0)
+pumps$county_id <- as.numeric(as.character(pumps$IN_county.COUNTYFP))
+summary(pumps$in_county)
+
+#Append nearest county variables
+pumps <- cbind(pumps, nearestCounty, nearestCounty_ID, nearestCounty_dist_km)
+summary(pumps[pumps$in_county==0,]$nearestCounty_dist_km)
+
+#Plot points in counties
+ggplot() + 
+  geom_polygon(data=CAoutline, aes(x=long, y=lat, group=group), 
+               color="grey30", fill=NA, alpha=1) +
+  geom_polygon(data=counties, aes(x=long, y=lat, group=group), 
+               color="green", fill=NA, alpha=1) +
+  geom_point(data=pumps[pumps$in_county==1,], aes(x=longitude, y=latitude), color=rgb(0,0,1), shape=19, 
+             alpha=1, size=1) 
+
+#Plot points NOT in counties // all are not in California
+ggplot() + 
+  geom_polygon(data=CAoutline, aes(x=long, y=lat, group=group), 
+               color="grey30", fill=NA, alpha=1) +
+  geom_polygon(data=counties, aes(x=long, y=lat, group=group), 
+               color="green", fill=NA, alpha=1) +
+  geom_point(data=pumps[(pumps$in_county==0),], aes(x=longitude, y=latitude), color=rgb(0,0,1), shape=19, 
+             alpha=1, size=1) 
+
+#Plot points NOT in counties that are in APEP, and are <2km from nearest county // 4 pumps in the ocean...
+ggplot() + 
+  geom_polygon(data=CAoutline, aes(x=long, y=lat, group=group), 
+               color="grey30", fill=NA, alpha=1) +
+  geom_polygon(data=counties, aes(x=long, y=lat, group=group), 
+               color="green", fill=NA, alpha=1) +
+  geom_point(data=pumps[(pumps$in_county==0 & pumps$nearestCounty_dist_km<2),], aes(x=longitude, y=latitude), color=rgb(0,0,1), shape=19, 
+             alpha=1, size=1) 
+
+#Drop extraneous variables
+pumps <- pumps[c("latlon_group","pump_lat","pump_long","longitude","latitude",
+                 "county","in_county","county_id")]
+
+#Export results to txt
+filename <- "apep_pump_coord_polygon_counties.txt"
+write.table(pumps, file=filename , row.names=FALSE, col.names=TRUE, sep="%", quote=FALSE, append=FALSE)
+
+
