1e36c6130ed76bc1252a1e7ce956dea8239da25a
lpreonas <lpreonas@gmail.com>
2019-01-02 12:11:41 -0800

    finished first pass at externality calcualtions

--- analyze/ANALYZE_externality_calcs.R
+++ analyze/ANALYZE_externality_calcs.R
@@ -3,8 +3,6 @@
 ################################################ 
 rm(list = ls())
 
-#Data downloaded from: https://data.cnra.ca.gov/dataset/ca-bulletin-118-groundwater-basins
-
 # install.packages("ggmap")
 # install.packages("ggplot2")
 # install.packages("gstat")
@@ -60,6 +58,9 @@ wbasn@data$area_km2 <- area(wbasn)/1000000
 data <- wbasn@data
 summary(data$area_km2)
 
+#Join sub-basin polygons into basin polygons
+#wbasn2 <- unionSpatialPolygons(wbasn,wbasn@data$Basin_Numb)
+
 
 ##########################################################################
 ### 2. Import panel data of SPs locations, pump specs, and water p & q ###
@@ -91,26 +92,23 @@ panel_sf_june <- panel_sf[panel_sf$month==6,]
 panel_sf_july <- panel_sf[panel_sf$month==7,]
 
 # Step 2: establish distance radii
-radii <- c(1,2,5,10,20) # miles
-radii <- radii*1609.344  # convert to meters, the units of the SF objects
+radii_miles <- c(1,2,5,10,20,30,40) # miles
+radii <- radii_miles*1609.344  # convert to meters, the units of the SF objects
+r <- length(radii)
 
 # Step 3: Draw circles around each unit
-circles1 <- st_buffer(panel_sf_june, dist=radii[1])
-circles2 <- st_buffer(panel_sf_june, dist=radii[2])
-circles3 <- st_buffer(panel_sf_june, dist=radii[3])
-circles4 <- st_buffer(panel_sf_june, dist=radii[4])
-circles5 <- st_buffer(panel_sf_june, dist=radii[5])
+for (i in 1:r){
+  assign(paste0("circles",i), st_buffer(panel_sf_june, dist=radii[i]))
+}  
 
 # Step 4: Intersect circles with basin polygons (COME BACK TO THIS)
 # wbasn_by_sp merge
 # circles1 <- st_intersection(wbasn_by_sp, circles1)
 
 # Step 5: Calculate area of each circle (sq meters)
-areas1 <- st_area(circles1)
-areas2 <- st_area(circles2)
-areas3 <- st_area(circles3)
-areas4 <- st_area(circles4)
-areas5 <- st_area(circles5)
+for (i in 1:r){
+  assign(paste0("areas",i), st_area(get(paste0("circles",i))))
+}  
 
 # Step 6: define groundwater variables and size of decrease in pumping for each unit i
 stub      <- "rast_dd_mth_2SP"
@@ -119,6 +117,9 @@ Q_var     <- paste0("af_",stub)
 kwhaf_var <- paste0("kwhaf_",stub)
 swl_var   <- paste0("gw_",gsub("_dd_","_depth_",stub))
 dd_var    <- ifelse(grepl("ddhat",stub),paste0("ddhat_",gsub("_dd_","_",stub)),"drwdwn_apep")
+tdh_var   <- paste0("tdh_",stub)
+ope_var   <- paste0("ope_",stub)
+eps_var   <- "elast_water"
 
 # Step 7: Calculate unit i's lost CS (for June) from pumping less
 dCS_i          <- panel_sf_june[[P_var]]
@@ -126,7 +127,7 @@ dCS_i          <- as.data.frame(dCS_i)
 names(dCS_i)   <- "P_old"
 dCS_i$Q_old    <- panel_sf_june[[Q_var]]
 dCS_i$delta_Q  <- ifelse(dCS_i$Q_old>1,1,NaN) #start with 1 AF, and see what happens\
-dCS_i$eps      <- panel_sf_june$elast_water
+dCS_i$eps      <- panel_sf_june[[eps_var]]
 dCS_i$Q_new    <- dCS_i$Q_old - dCS_i$delta_Q
 dCS_i$P_new    <- (dCS_i$Q_new/dCS_i$Q_old)^(1/(dCS_i$eps))*dCS_i$P_old
 dCS_i$integral <- (dCS_i$Q_old/(dCS_i$eps+1))*((dCS_i$P_old^(-dCS_i$eps))*(dCS_i$P_new^(dCS_i$eps+1))-dCS_i$P_old)
@@ -135,210 +136,99 @@ dCS_i$rectangle<- dCS_i$Q_new * (dCS_i$P_new - dCS_i$P_old)
 dCS_i$dCS_i    <- -(dCS_i$integral - dCS_i$rectangle)
 
 # Step 8: Calculate how much the water level rises in each circle when unit i pumps less
-delta_swl1 <- dCS_i$delta_Q/(areas1/4046.856) #convert acres from sq meters to acres
-delta_swl2 <- dCS_i$delta_Q/(areas2/4046.856) #convert acres from sq meters to acres
-delta_swl3 <- dCS_i$delta_Q/(areas3/4046.856) #convert acres from sq meters to acres
-delta_swl4 <- dCS_i$delta_Q/(areas4/4046.856) #convert acres from sq meters to acres
-delta_swl5 <- dCS_i$delta_Q/(areas5/4046.856) #convert acres from sq meters to acres
+for (i in 1:r){
+  assign(paste0("delta_swl",i), dCS_i$delta_Q/(get(paste0("areas",i))/4046.856)) #convert from sq meters to acres
+}  
 
-# Step 9: Flag July SPs in each June circle, making separate data frame with J units
-#in polygon function to subset SPs
+# Step 9: Grab set J of July points in each i-specific June circle
+for (i in 1:r){
+  assign(paste0("pts_in_circles",i), st_intersects(get(paste0("circles",i)),panel_sf_july)) 
+}  
 
-# Step 10: Impose a uniform delta SWL, and calculate effect on P_water for all J units
+# Step 10: Remove unit i from each i-specific set of neighbors J
+sp_matches <- match(panel_sf_june$sp_group,panel_sf_july$sp_group)
+for (i in 1:r){
+  assign(paste0("pts_in_circles",i), mapply(function(x,y) setdiff(x,y), get(paste0("pts_in_circles",i)), sp_matches)) 
+}  
 
+# Step 11: For each i-specific et of J neighbors, subset the correct rows of July panel 
+for (i in 1:r){
+  assign(paste0("dfs_circles",i), lapply(get(paste0("pts_in_circles",i)), function(x) panel_sf_july[x,])) 
+}  
 
-# Step 11: Calculate increase in CS (for July) for all J units
+# Step 12: Impose i-specific uniform increases in depth, and calculate new SWL for each unit j in each set J
+for (i in 1:r){
+  swl_news <- mapply(function(x,y) x[[swl_var]] - as.numeric(y), 
+                     get(paste0("dfs_circles",i)),get(paste0("delta_swl",i))
+              )
+  assign(paste0("dfs_circles",i), Map(cbind, get(paste0("dfs_circles",i)), swl_new = swl_news))
 
-# Step 12: Sum all changes in CS_j
-
-# Step 13: Store changes in CS_j, CS_i, and number of J units in circle
-
-
-
-
-i <- 197
-
-
-
-
-
-
-
-
-
-#Assign each lat/lon to the water basin polygon it's contained in
-panel@data$IN_wbasn <- over(prems, wbasn)
-
-#Reproject everything into planar coordinates
-utmStr <- "+proj=utm +zone=%d +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0"
-crs <- CRS(sprintf(utmStr, 10))
-wbasnUTM <- spTransform(wbasn, crs) #reproject into planar coordinates, because that's what the distance function uses
-premsUTM <- spTransform(prems, crs)
-
-#Create empy vectors to loop over, to calculate distance to a water basin
-n <- nrow(prems@data)
-nearestwbasn_ID <- numeric(n)
-nearestwbasn_dist_km <- numeric(n)
-
-#Create vector of only those observations where water basin is missing
-missings <- c(1:n)[is.na(prems@data$IN_wbasn$OBJECTID)]
-
-#Calculate distance to a water basin polygon, for lat/lons not contained in a polygon
-for (j in seq_along(missings)) {
-  i <- missings[j]
-  temp <- wbasnUTM@data[which.min(gDistance(premsUTM[i,], wbasnUTM, byid=TRUE)),]
-  nearestwbasn_ID[i]   <- as.numeric(as.character(temp$OBJECTID))
-  nearestwbasn_dist_km[i] <- min(gDistance(premsUTM[i,], wbasnUTM, byid=TRUE))/1000
+  # Something's getting systematically messed up in the conversion from depth to lift, 
+  # either from how I've averaged the "tdh_adder" variable across multiple pumps in an SP-month
+  # or from how I've interpolated it across time. This makes not sense to me, because it's all additive!
+  # As a stop-gap, I'm just skipping straight to TDH (i.e. lift) and subtracting delta_swl from that...
+  # Need to come back and fix this!
+  tdh_news <- mapply(function(x,y) x[[tdh_var]] - as.numeric(y), 
+                     get(paste0("dfs_circles",i)),get(paste0("delta_swl",i))
+  )
+  assign(paste0("dfs_circles",i), Map(cbind, get(paste0("dfs_circles",i)), tdh_new = tdh_news))
 }
 
-#Convert back to regular dataframe
-prems <- as.data.frame(prems)
+# Step 13: Calculate depth_new --> lift_new --> kwhaf_new --> P_new --> dCS_j
+for (i in 1:r){
+  
+  #lift_news <- lapply(get(paste0("dfs_circles",i)), function(x) x[["swl_new"]] + x[[dd_var]] + x[["tdh_adder"]])
+  lift_news <- lapply(get(paste0("dfs_circles",i)), function(x) x[["tdh_new"]])
+  
+  kwhaf_news <- mapply(function(x,y) y * 102.26728 / x[[ope_var]], get(paste0("dfs_circles",i)), lift_news)
 
-#Assign in_wbasn dummy and store water basin name and area
-prems$wbasn <- prems$IN_wbasn.Basin_Name
-prems$in_wbasn <- as.numeric(is.na(prems$wbasn)==0)
-prems$wbasn_id <- as.numeric(as.character(prems$IN_wbasn.OBJECTID))
-summary(prems$in_wbasn)
-summary(prems$in_wbasn[prems$bad_geocode_flag==0])
-summary(prems$in_wbasn[prems$bad_geocode_flag==0 & prems$pull=="20180719"])
-
-#Append nearest water basin variables
-prems <- cbind(prems, nearestwbasn_ID, nearestwbasn_dist_km)
-summary(prems[prems$in_wbasn==0,]$nearestwbasn_dist_km)
-summary(prems[prems$in_wbasn==0 & prems$bad_geocode_flag==0 & prems$pull=="20180719",]$nearestwbasn_dist_km)
-
-#Plot points in water basins
-ggplot() + 
-  geom_polygon(data=CAoutline, aes(x=long, y=lat, group=group), 
-               color="grey30", fill=NA, alpha=1) +
-  geom_polygon(data=wbasn, aes(x=long, y=lat, group=group), 
-               color="green", fill=NA, alpha=1) +
-  geom_point(data=prems[prems$in_wbasn==1,], aes(x=longitude, y=latitude), color=rgb(0,0,1), shape=19, 
-             alpha=1, size=1) 
-
-#Plot points NOT in water basins
-ggplot() + 
-  geom_polygon(data=CAoutline, aes(x=long, y=lat, group=group), 
-               color="grey30", fill=NA, alpha=1) +
-  geom_polygon(data=wbasn, aes(x=long, y=lat, group=group), 
-               color="green", fill=NA, alpha=1) +
-  geom_point(data=prems[(prems$in_wbasn==0 & prems$bad_geocode_flag==0),], aes(x=longitude, y=latitude), color=rgb(0,0,1), shape=19, 
-             alpha=1, size=1) 
-
-#Plot points NOT in water basins that are in APEP
-ggplot() + 
-  geom_polygon(data=CAoutline, aes(x=long, y=lat, group=group), 
-               color="grey30", fill=NA, alpha=1) +
-  geom_polygon(data=wbasn, aes(x=long, y=lat, group=group), 
-               color="green", fill=NA, alpha=1) +
-  geom_point(data=prems[(prems$in_wbasn==0 & prems$bad_geocode_flag==0 & prems$pull=="20180719"),], aes(x=longitude, y=latitude), color=rgb(0,0,1), shape=19, 
-             alpha=1, size=1) 
-
-#Plot points NOT in water basins that are in APEP, and are <2km from nearest water basin
-ggplot() + 
-  geom_polygon(data=CAoutline, aes(x=long, y=lat, group=group), 
-               color="grey30", fill=NA, alpha=1) +
-  geom_polygon(data=wbasn, aes(x=long, y=lat, group=group), 
-               color="green", fill=NA, alpha=1) +
-  geom_point(data=prems[(prems$in_wbasn==0 & prems$bad_geocode_flag==0 & prems$pull=="20180719" & prems$nearestwbasn_dist_km<2),], aes(x=longitude, y=latitude), color=rgb(0,0,1), shape=19, 
-             alpha=1, size=1) 
-
-#Drop extraneous variables
-prems <- prems[c("sp_uuid","prem_lat","prem_long","longitude","latitude",
-                 "bad_geocode_flag","pull","wbasn","in_wbasn","wbasn_id",
-                 "nearestwbasn_ID", "nearestwbasn_dist_km")]
-
-#Export results to txt
-filename <- "pge_prem_coord_polygon_wbasn.txt"
-write.table(prems, file=filename , row.names=FALSE, col.names=TRUE, sep="%", quote=FALSE, append=FALSE)
-
-
-###############################################
-### 3. Assign APEP lat/lons to water basins ###
-###############################################
-
-#Read APEP coordinates
-setwd("S:/Matt/ag_pump/data/misc")
-pumps <- read.delim2("apep_pump_coord.txt",header=TRUE,sep=",",stringsAsFactors=FALSE)
-pumps$longitude <- as.numeric(pumps$pump_lon)
-pumps$latitude <- as.numeric(pumps$pump_lat)
-
-#Convert to SpatialPointsDataFrame
-coordinates(pumps) <- ~ longitude + latitude
-proj4string(pumps) <- proj4string(wbasn)
-
-#Assign each lat/lon to the water basin polygon it's contained in
-pumps@data$IN_wbasn <- over(pumps, wbasn)
-
-#Reproject everything into planar coordinates
-utmStr <- "+proj=utm +zone=%d +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0"
-crs <- CRS(sprintf(utmStr, 10))
-wbasnUTM <- spTransform(wbasn, crs) #reproject into planar coordinates, because that's what the distance function uses
-pumpsUTM <- spTransform(pumps, crs)
-
-#Create empy vectors to loop over, to calculate distance to a water basin
-n <- nrow(pumps@data)
-nearestwbasn_ID <- numeric(n)
-nearestwbasn_dist_km <- numeric(n)
-
-#Create vector of only those observations where water basin is missing
-missings <- c(1:n)[is.na(pumps@data$IN_wbasn$OBJECTID)]
-
-#Calculate distance to a water basin polygon, for lat/lons not contained in a polygon
-for (j in seq_along(missings)) {
-  i <- missings[j]
-  temp <- wbasnUTM@data[which.min(gDistance(pumpsUTM[i,], wbasnUTM, byid=TRUE)),]
-  nearestwbasn_ID[i]   <- as.numeric(as.character(temp$OBJECTID))
-  nearestwbasn_dist_km[i] <- min(gDistance(pumpsUTM[i,], wbasnUTM, byid=TRUE))/1000
+  P_news <- mapply(function(x,y) x[["mean_p_kwh"]] * y, get(paste0("dfs_circles",i)), kwhaf_news)
+  
+  dCS_js <- mapply(function(x,y) {
+                   -(x[[Q_var]]/(x[[eps_var]]+1))*((x[[P_var]]^(-x[[eps_var]]))*(y^(x[[eps_var]]+1))-x[[P_var]])
+                    }, get(paste0("dfs_circles",i)), P_news
+                   )
+  
+  assign(paste0("dfs_circles",i), Map(cbind, get(paste0("dfs_circles",i)), lift_new = lift_news))
+  assign(paste0("dfs_circles",i), Map(cbind, get(paste0("dfs_circles",i)), kwhaf_new = kwhaf_news))
+  assign(paste0("dfs_circles",i), Map(cbind, get(paste0("dfs_circles",i)), P_new = P_news))
+  assign(paste0("dfs_circles",i), Map(cbind, get(paste0("dfs_circles",i)), dCS_j = dCS_js))
 }
 
-#Convert back to regular dataframe
-pumps <- as.data.frame(pumps)
+# Step 14: Store outputs -- sum (positive) changes in CS_j, avg price decrease, number of (positive) units
+for (i in 1:r){
+  
+  sum_dCS_js <- lapply(get(paste0("dfs_circles",i)), function(x) sum(x[["dCS_j"]]))
+  
+  sum_dCS_j_poss <- lapply(get(paste0("dfs_circles",i)), function(x) sum(x[x[["dCS_j"]]>0,][["dCS_j"]]))
+                          
+  mean_dP_js <- lapply(get(paste0("dfs_circles",i)), function(x) mean(x[[P_var]] - x[["P_new"]]))
 
-#Assign in_wbasn dummy and store water basin name and area
-pumps$wbasn <- pumps$IN_wbasn.Basin_Name
-pumps$in_wbasn <- as.numeric(is.na(pumps$wbasn)==0)
-pumps$wbasn_id <- as.numeric(as.character(pumps$IN_wbasn.OBJECTID))
-summary(pumps$in_wbasn)
+  n_js <- lapply(get(paste0("dfs_circles",i)), function(x) nrow(x))
+  
+  n_j_poss <- lapply(get(paste0("dfs_circles",i)), function(x) nrow(x[x[[Q_var]]>0,]))
+  
+  assign(paste0("out_circles",i), Map(cbind, sum_dCS_j = sum_dCS_js,  sum_dCS_j_pos = sum_dCS_j_poss))
+  assign(paste0("out_circles",i), Map(cbind, get(paste0("out_circles",i)), mean_dP_j = mean_dP_js))
+  assign(paste0("out_circles",i), Map(cbind, get(paste0("out_circles",i)), n_j = n_js))
+  assign(paste0("out_circles",i), Map(cbind, get(paste0("out_circles",i)), n_j_pos = n_j_poss))
+}  
+  
+# Step 15: Merge j outputs into dCS_i data frame
+for (i in 1:r){
+  out_df <- as.data.frame(Reduce(rbind, get(paste0("out_circles",i))))
+  j <- radii_miles[i]
+  names(out_df) <- sapply(names(out_df), function(x) paste0(x,j))
+  assign(paste0("out_df",i), out_df)
+}
+ids <- as.data.frame(panel_sf_june)
+ids <- ids[ names(ids)[names(ids) %in% c("sp_uuid","modate","basin_group","in_regs","basin_dist",
+                                         "basin_dist_miles","basin_id","basin_name")] ]
+out_df <- cbind(ids,dCS_i)
+for (i in 1:r){
+  out_df <- cbind(out_df,get(paste0("out_df",i)))
+}
 
-#Append nearest water basin variables
-pumps <- cbind(pumps, nearestwbasn_ID, nearestwbasn_dist_km)
-summary(pumps[pumps$in_wbasn==0,]$nearestwbasn_dist_km)
-
-#Plot points in water basins
-ggplot() + 
-  geom_polygon(data=CAoutline, aes(x=long, y=lat, group=group), 
-               color="grey30", fill=NA, alpha=1) +
-  geom_polygon(data=wbasn, aes(x=long, y=lat, group=group), 
-               color="green", fill=NA, alpha=1) +
-  geom_point(data=pumps[pumps$in_wbasn==1,], aes(x=longitude, y=latitude), color=rgb(0,0,1), shape=19, 
-             alpha=1, size=1) 
-
-#Plot points NOT in water basins
-ggplot() + 
-  geom_polygon(data=CAoutline, aes(x=long, y=lat, group=group), 
-               color="grey30", fill=NA, alpha=1) +
-  geom_polygon(data=wbasn, aes(x=long, y=lat, group=group), 
-               color="green", fill=NA, alpha=1) +
-  geom_point(data=pumps[(pumps$in_wbasn==0),], aes(x=longitude, y=latitude), color=rgb(0,0,1), shape=19, 
-             alpha=1, size=1) 
-
-#Plot points NOT in water basins that are in APEP, and are <2km from nearest water basin
-ggplot() + 
-  geom_polygon(data=CAoutline, aes(x=long, y=lat, group=group), 
-               color="grey30", fill=NA, alpha=1) +
-  geom_polygon(data=wbasn, aes(x=long, y=lat, group=group), 
-               color="green", fill=NA, alpha=1) +
-  geom_point(data=pumps[(pumps$in_wbasn==0 & pumps$nearestwbasn_dist_km<2),], aes(x=longitude, y=latitude), color=rgb(0,0,1), shape=19, 
-             alpha=1, size=1) 
-
-#Drop extraneous variables
-pumps <- pumps[c("latlon_group","pump_lat","pump_long","longitude","latitude",
-                 "wbasn","in_wbasn","wbasn_id",
-                 "nearestwbasn_ID", "nearestwbasn_dist_km")]
-
-#Export results to txt
-filename <- "apep_pump_coord_polygon_wbasn.txt"
-write.table(pumps, file=filename , row.names=FALSE, col.names=TRUE, sep="%", quote=FALSE, append=FALSE)
-
-
+# Step 16: Export results
+filename <- paste0("externality_calcs_june2016_",stub,".csv")
+write.csv(out_df, file=filename , row.names=FALSE, quote=FALSE)
