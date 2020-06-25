7c2bf81a0b8b064ebbaeac6f3f28a6d710deb48d
lpreonas <lpreonas@gmail.com>
2020-06-25 09:05:31 -0700

    file to execute polygon-to-polygon merge for CLUs to counties. this is necessary, because as it turns out, a lot of the CLUs actually don't reside in the county they are tagged in in the raw data (and as would be implied by their unique identifiers).
    this commit adds a chunk of code to loop over unmatched CLUs, find the nearest county polygon, and store its name and distance (in meters). have to loop this because for some reason running the matrix version of sf::st_distance() crashes R

--- build/GIS_Data_Build/BUILD_gis_clu_counties.R
+++ build/GIS_Data_Build/BUILD_gis_clu_counties.R
@@ -1,6 +1,9 @@
 # Created by Chinmay Lohani in November 2019
 # Find the intersections between counties and CLUs
 # Additional notes exist on validity of intersections dropped in Dbox "Chinmay_notes_spatial_Dec19.pdf"
+##############################################################
+#  Script to execute polygon-to-polygon merge: CLU to county #
+############################################################## 
 
 rm(list = ls())
 
@@ -18,16 +21,13 @@ library(sf)
 library(assertr)
 library(lwgeom)
 
-if(Sys.getenv("USERNAME") == "clohani"){
-  root_gh <- "C:/Users/clohani/OneDrive/Documents/github/Ag-EE"
-}
-
 m2_to_acre <- 0.000247105
 
-path_counties <- "T:/Projects/Pump Data/data/spatial/Counties"
-path_clu <- "T:/Projects/Pump Data/data/cleaned_spatial/CLU/clu_poly"
-path_output <- "C:/Users/clohani/Desktop/Code_Chinmay"
-path_counties <- "T:/Projects/Pump Data/data/spatial/Counties"
+path <- "T:/Projects/Pump Data/"
+
+path_counties <- paste0(path,"data/spatial/Counties")
+path_clu <- paste0(path,"data/cleaned_spatial/CLU/clu_poly")
+path_output <- paste0(path,"data/misc")
 
 #create sf objects for CLUs and counties
 clu <- st_read(file.path(path_clu, "clu_poly.shp"))
@@ -58,7 +58,6 @@ inter <- st_intersection(lwgeom::st_make_valid(clu), lwgeom::st_make_valid(count
 
 #find area of these things
 #create a variable which sums total area of these things
-
 inter <- inter %>% 
   mutate(IntAcres = as.numeric(st_area(.)) * m2_to_acre) %>%
   st_set_geometry(NULL) %>%
@@ -66,39 +65,31 @@ inter <- inter %>%
   mutate(tot_int_area= sum(IntAcres)) %>%
   ungroup
 
-## save outputs
-#saveRDS(inter, file.path(path_output, "CLU_counties.RDS"))
-#inter <- readRDS("C:/Users/clohani/OneDrive/Documents/Intersected_data.rds")
-## check if the total area of intersections is atleast as much as the CLU area
-#inter %>%
-#  verify(tot_area>CLUAcres)
 
-# define a variable that tells whether intersected or not, take union with original, select largest intersected
+#subset CLUs to isolate non-matches
+clu_matched <- as.data.frame(inter$CLU_ID)
+names(clu_matched) <- "CLU_ID"
+clu_unmatched <- anti_join(clu, clu_matched, by="CLU_ID")
 
-union_clus <- inter %>%
-  mutate(has_intersection=1) %>%
-  bind_rows(valid_clu,.)
+#for unmatched CLUs, calculate distance to nearest county
+for (i in 1:nrow(clu_unmatched)) {
+  d_all <- st_distance(lwgeom::st_make_valid(clu_unmatched)[i,], lwgeom::st_make_valid(counties))
+  d_min <- min(d_all)
+  d_which <- which(d_min==d_all)
+  d_nearest <- st_drop_geometry(counties[d_which,])
+  d_out <- cbind(st_drop_geometry(clu_unmatched[i,]),d_min,d_nearest)
+  if (i==1){
+    out_nearest <- d_out
+  }
+  else {
+    out_nearest <- rbind(out_nearest,d_out)  
+  }
+  print(i)
+}  
+summary(out_nearest$d_min)
 
-sliced_union_clus <- union_clus %>%
-  group_by(CLU_ID) %>%
-  arrange(desc(IntAcres), .by_group = TRUE) %>%
-  slice(1)%>%
-  ungroup()
+#export results
+outfile <- full_join(inter,out_nearest, by="CLU_ID")
+file <- paste0(path_output,"/CLU_counties.csv")
+write.csv(outfile, file, sep=",", row.names=FALSE, col.names=TRUE)
 
-# code for summarising things 
-
-union_clus %>% 
-  mutate(all=1) %>%
-  group_by(all) %>%
-  summarise(`1%`=quantile(frac_area, probs=0.01, na.rm = TRUE),
-            `5%`=quantile(frac_area, probs=0.5, na.rm = TRUE),
-            `10%`=quantile(frac_area, probs=0.10, na.rm = TRUE),
-            `25%`=quantile(frac_area, probs=0.25, na.rm = TRUE),
-            `50%`=quantile(frac_area, probs=0.5, na.rm = TRUE),
-            `75%`=quantile(frac_area, probs=0.75, na.rm = TRUE),
-            `90%`=quantile(frac_area, probs=0.9, na.rm = TRUE),
-            `99%`=quantile(frac_area, probs=0.99, na.rm = TRUE),
-            avg=mean(frac_area, na.rm = TRUE)
-  )
-
-saveRDS(union_clus, file.path(path_output, "Intersected_CLU_county.rds"))
