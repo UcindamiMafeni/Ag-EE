2cd2bbde5630d06240f64c7a34beb6746819c104
lpreonas <lpreonas@gmail.com>
2020-07-23 11:41:40 -0700

    added chunk of code to extract monthly/quarterly groundwater levels for CLU centroids

--- build/Combined_Data_Build/BUILD_com_gw_depth_extract.R
+++ build/Combined_Data_Build/BUILD_com_gw_depth_extract.R
@@ -11,6 +11,7 @@ library(maptools)
 library(rgdal)
 library(raster)
 library(dplyr)
+library(sf)
 
 #######################################################################################################
 ### 1. Extract groundwater depths for each PGE SP and APEP pump, from each monthly/quarterly raster ###
@@ -21,7 +22,7 @@ path <- "T:/Projects/Pump Data/"
 setwd(paste0(path,"data/misc"))
 
 #Load monthly/quarterly rasters
-load("temp_gw_idw_rasters.RData")
+load("gw_idw_rasters.RData")
 
 #Read PGE coordinates
 prems <- read.delim2("pge_prem_coord_3pulls.txt",header=TRUE,sep=",",stringsAsFactors=FALSE)
@@ -209,7 +210,7 @@ path <- "T:/Projects/Pump Data/"
 setwd(paste0(path,"data/misc"))
 
 #Load monthly/quarterly rasters
-load("temp_gw_idw_rasters_SJ.RData")
+load("gw_idw_rasters_SJ.RData")
 
 #Read PGE coordinates
 prems <- read.delim2("pge_prem_coord_3pulls.txt",header=TRUE,sep=",",stringsAsFactors=FALSE)
@@ -396,7 +397,7 @@ path <- "T:/Projects/Pump Data/"
 setwd(paste0(path,"data/misc"))
 
 #Load monthly/quarterly rasters
-load("temp_gw_idw_rasters_SAC.RData")
+load("gw_idw_rasters_SAC.RData")
 
 #Read PGE coordinates
 prems <- read.delim2("pge_prem_coord_3pulls.txt",header=TRUE,sep=",",stringsAsFactors=FALSE)
@@ -583,7 +584,7 @@ path <- "T:/Projects/Pump Data/"
 setwd(paste0(path,"data/misc"))
 
 #Load monthly/quarterly rasters
-load("temp_gw_idw_rasters.RData")
+load("gw_idw_rasters.RData")
 
 #Read SCE coordinates
 socal <- read.delim2("sce_prem_coord_1pull.txt",header=TRUE,sep=",",stringsAsFactors=FALSE)
@@ -719,5 +720,161 @@ for (yq in levels(gwqtr$qtr)) {
 #Export quarterly SP and pump results to CSV
 filename <- paste0("socal_gw_depths_from_rasters_qtr.csv")
 write.csv(socal, file=filename , row.names=FALSE, quote=FALSE)
+
+
+
+###############################################################################################
+### 5. Extract groundwater depths for each CLU centroid, from each monthly/quarterly raster ###
+###############################################################################################
+
+rm(list = ls())
+path <- "T:/Projects/Pump Data/"
+setwd(paste0(path,"data/misc"))
+
+#Load monthly/quarterly rasters
+load("gw_idw_rasters.RData")
+
+#Read CLU coordinates
+clu_sf <- readRDS(file=paste0(path,"data/cleaned_spatial/CLU/clu_centroids.RDS"))
+latlon <- do.call(rbind, st_geometry(clu_sf)) %>%  as_tibble() %>% setNames(c("lon","lat")) %>% as.data.frame(stringsAsFactors=FALSE)
+coords <- as.data.frame(cbind(as.data.frame(clu_sf$rowID),latlon))
+coords <- coords[is.na(coords$lat)==0,]
+coords <- coords[is.na(coords$lon)==0,]
+names(coords) <- c("rowID","lon","lat")
+class(coords)
+
+clus <- coords
+clus$x <- as.numeric(clus$lon)
+clus$y <- as.numeric(clus$lat)
+
+#Create SpatialPointsDataFrame analogs
+clus_2 <- clus
+coordinates(clus_2) <- ~ x + y
+proj4string(clus_2) <- CRS("+proj=longlat +datum=WGS84 +units=m +ellps=WGS84 +towgs84=0,0,0")
+
+#Loop over months to extract monthly groundwater depths
+for (ym in levels(gwmth$modate)) {
+  
+  #Store ym's monthly rasters
+  temp_ra1 <- get(paste0("gwmth_rast_1_",ym))
+  temp_ra2 <- get(paste0("gwmth_rast_2_",ym))
+  temp_ra3 <- get(paste0("gwmth_rast_3_",ym))
+  
+  #Create names for extracted values from rasters
+  name_1s <- paste0("depth_1s_",ym)
+  name_1b <- paste0("depth_1b_",ym)
+  name_2s <- paste0("depth_2s_",ym)
+  name_2b <- paste0("depth_2b_",ym)
+  name_3s <- paste0("depth_3s_",ym)
+  name_3b <- paste0("depth_3b_",ym)
+  
+  #Extract groundwater dephts from rasters (CLU centroids)
+  clus[[name_1s]] <- extract(temp_ra1,clus_2,method='simple'  ,df=TRUE)$layer
+  clus[[name_1b]] <- extract(temp_ra1,clus_2,method='bilinear',df=TRUE)$layer
+  clus[[name_2s]] <- extract(temp_ra2,clus_2,method='simple'  ,df=TRUE)$layer
+  clus[[name_2b]] <- extract(temp_ra2,clus_2,method='bilinear',df=TRUE)$layer
+  clus[[name_3s]] <- extract(temp_ra3,clus_2,method='simple'  ,df=TRUE)$layer
+  clus[[name_3b]] <- extract(temp_ra3,clus_2,method='bilinear',df=TRUE)$layer
+  
+  #Store ym's monthly data frames
+  temp_df1 <- get(paste0("gwmth_1_",ym))
+  temp_df2 <- get(paste0("gwmth_2_",ym))
+  temp_df3 <- get(paste0("gwmth_3_",ym))
+  
+  #Reproject spatial data frames to put units in meters
+  temp_df1 <- spTransform(temp_df1,"+proj=longlat +datum=WGS84 +units=m +ellps=WGS84 +towgs84=0,0,0")
+  temp_df2 <- spTransform(temp_df2,"+proj=longlat +datum=WGS84 +units=m +ellps=WGS84 +towgs84=0,0,0")
+  temp_df3 <- spTransform(temp_df3,"+proj=longlat +datum=WGS84 +units=m +ellps=WGS84 +towgs84=0,0,0")
+  
+  #Create names for distances to nearest groundwater reading
+  name_1d <- paste0("distkm_1_",ym)
+  name_2d <- paste0("distkm_2_",ym)
+  name_3d <- paste0("distkm_3_",ym)
+  
+  #Calculate distance to nearest groundwater reading (CLU centroids)
+  clus[[name_1d]] <- do.call(pmin, as.data.frame(pointDistance(clus_2,temp_df1,lonlat=TRUE)))/1000
+  clus[[name_2d]] <- do.call(pmin, as.data.frame(pointDistance(clus_2,temp_df2,lonlat=TRUE)))/1000
+  clus[[name_3d]] <- do.call(pmin, as.data.frame(pointDistance(clus_2,temp_df3,lonlat=TRUE)))/1000
+  
+  #Intermediate output
+  print(paste(ym,"  ",Sys.time()))
+  
+  #Remove ym's monthly rasters and data frames that we no longer need, to save memory
+  rm(list=c(paste0("gwmth_rast_1_",ym)))
+  rm(list=c(paste0("gwmth_rast_2_",ym)))
+  rm(list=c(paste0("gwmth_rast_3_",ym)))
+  rm(list=c(paste0("gwmth_1_",ym)))
+  rm(list=c(paste0("gwmth_2_",ym)))
+  rm(list=c(paste0("gwmth_3_",ym)))
+  
+}  
+
+#Export monthly CLU centroid results to CSV
+filename <- paste0("clus_gw_depths_from_rasters_mth.csv")
+write.csv(clus, file=filename , row.names=FALSE, quote=FALSE)
+
+#Remove monthly columns from output dataframes
+clus <- clus %>% select(c(sp_uuid,prem_lat,prem_long,bad_geocode_flag,missing_geocode_flag,pull,x,y))
+
+#Loop over quarters to extract quarterly groundwater depths
+for (yq in levels(gwqtr$qtr)) {
+  
+  #Store yq's quarterly rasters
+  temp_ra1 <- get(paste0("gwqtr_rast_1_",yq))
+  temp_ra2 <- get(paste0("gwqtr_rast_2_",yq))
+  temp_ra3 <- get(paste0("gwqtr_rast_3_",yq))
+  
+  #Create names for extracted values from rasters
+  name_1s <- paste0("depth_1s_",yq)
+  name_1b <- paste0("depth_1b_",yq)
+  name_2s <- paste0("depth_2s_",yq)
+  name_2b <- paste0("depth_2b_",yq)
+  name_3s <- paste0("depth_3s_",yq)
+  name_3b <- paste0("depth_3b_",yq)
+  
+  #Extract groundwater dephts from rasters (CLU centroids)
+  clus[[name_1s]] <- extract(temp_ra1,clus_2,method='simple'  ,df=TRUE)$layer
+  clus[[name_1b]] <- extract(temp_ra1,clus_2,method='bilinear',df=TRUE)$layer
+  clus[[name_2s]] <- extract(temp_ra2,clus_2,method='simple'  ,df=TRUE)$layer
+  clus[[name_2b]] <- extract(temp_ra2,clus_2,method='bilinear',df=TRUE)$layer
+  clus[[name_3s]] <- extract(temp_ra3,clus_2,method='simple'  ,df=TRUE)$layer
+  clus[[name_3b]] <- extract(temp_ra3,clus_2,method='bilinear',df=TRUE)$layer
+  
+  #Store yq's quarterly data frames
+  temp_df1 <- get(paste0("gwqtr_1_",yq))
+  temp_df2 <- get(paste0("gwqtr_2_",yq))
+  temp_df3 <- get(paste0("gwqtr_3_",yq))
+  
+  #Reproject spatial data frames to put units in meters
+  temp_df1 <- spTransform(temp_df1,"+proj=longlat +datum=WGS84 +units=m +ellps=WGS84 +towgs84=0,0,0")
+  temp_df2 <- spTransform(temp_df2,"+proj=longlat +datum=WGS84 +units=m +ellps=WGS84 +towgs84=0,0,0")
+  temp_df3 <- spTransform(temp_df3,"+proj=longlat +datum=WGS84 +units=m +ellps=WGS84 +towgs84=0,0,0")
+  
+  #Create names for distances to nearest groundwater reading
+  name_1d <- paste0("distkm_1_",yq)
+  name_2d <- paste0("distkm_2_",yq)
+  name_3d <- paste0("distkm_3_",yq)
+  
+  #Calculate distance to nearest groundwater reading (CLU centroids)
+  clus[[name_1d]] <- do.call(pmin, as.data.frame(pointDistance(clus_2,temp_df1,lonlat=TRUE)))/1000
+  clus[[name_2d]] <- do.call(pmin, as.data.frame(pointDistance(clus_2,temp_df2,lonlat=TRUE)))/1000
+  clus[[name_3d]] <- do.call(pmin, as.data.frame(pointDistance(clus_2,temp_df3,lonlat=TRUE)))/1000
+  
+  #Intermediate output
+  print(paste(yq,"  ",Sys.time()))
+  
+  #Remove yq's quarterly rasters and data drames that we no longer need, to save memory
+  rm(list=c(paste0("gwqtr_rast_1_",yq)))
+  rm(list=c(paste0("gwqtr_rast_2_",yq)))
+  rm(list=c(paste0("gwqtr_rast_3_",yq)))
+  rm(list=c(paste0("gwqtr_1_",yq)))
+  rm(list=c(paste0("gwqtr_2_",yq)))
+  rm(list=c(paste0("gwqtr_3_",yq)))
+  
+}  
+
+#Export quarterly CLU centroid results to CSV
+filename <- paste0("clus_gw_depths_from_rasters_qtr.csv")
+write.csv(clus, file=filename , row.names=FALSE, quote=FALSE)
 
 
